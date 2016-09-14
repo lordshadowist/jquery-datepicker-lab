73b7c52153ee055b268b7960f6758ef2ce3596f7
lordshadowist <shadowist@live.com>
2016-09-13 20:17:07 -0700

    Finish Step 3

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function daysLeft() {
 	var a = $( "#datepicker" ).datepicker('getDate').getTime();
 	// solutions for 9, 10, 11 and 12 go here:
 
-	// var b = _____
-	// var c = _____
-	// var d = _____
-
+	 var b = today.getTime();
+	 var c = 24 * 60 * 60 * 1000;
+	 var d = Math.ceil((a - b) / c);
+	 console.log(d);
 	// we will remove this later:
 	return a;
 }
